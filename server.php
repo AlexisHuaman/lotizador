508f4961a615d77e860af886d51894804428c6bf
Alexis <ahuamani@unjbg.edu.pe>
2024-09-24 12:32:18 -0500

    version 34

--- server.php
+++ server.php
@@ -1,14 +1,16 @@
 <?php
 // server.php
 
-// Mostrar errores para depuración
+// Habilitar el manejo de errores
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Iniciar el servidor PHP en el puerto definido por la variable de entorno PORT
-$port = getenv('PORT') ?: 8000; // Valor por defecto en caso de que PORT no esté definido
-$host = '0.0.0.0'; // Aceptar conexiones desde cualquier IP
+// Definir el puerto si no está definido
+$port = isset($_SERVER['PORT']) ? $_SERVER['PORT'] : 8080;
 
-// Iniciar el servidor
-echo "Servidor PHP corriendo en http://$host:$port\n";
-require 'vista/index.php'; // Incluir el archivo index.php
+// Iniciar el servidor embebido
+$host = '0.0.0.0';
+$docRoot = 'htdocs'; // Cambia esto a tu directorio de documentos, si es diferente
+$command = "php -S $host:$port -t $docRoot";
+
+system($command);
